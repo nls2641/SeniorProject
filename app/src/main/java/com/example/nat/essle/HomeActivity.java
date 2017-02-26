a36759084e75403d53b5de46d427a9064f9a98a4
Nat <nsrocds@gmail.com>
2017-02-26 14:39:56 -0800

    First commit

---
+++ app/src/main/java/com/example/nat/essle/HomeActivity.java
@@ -0,0 +1,32 @@
+package com.example.nat.essle;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class HomeActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayTranslatedActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+    public void learn(View view) {
+        Intent intent = new Intent(this, LearnActivity.class);
+
+        startActivity(intent);
+    }
+}
