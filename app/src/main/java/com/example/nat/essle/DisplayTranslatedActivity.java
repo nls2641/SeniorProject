eccb41e68a4340147f22968b1183a50b5984efc9
Nat <nsrocds@gmail.com>
2017-03-05 18:00:34 -0800

    Working on writing to file

--- app/src/main/java/com/example/nat/essle/DisplayTranslatedActivity.java
+++ app/src/main/java/com/example/nat/essle/DisplayTranslatedActivity.java
@@ -1,14 +1,19 @@
 package com.example.nat.essle;
 
 import android.content.Intent;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 
@@ -23,7 +28,7 @@ public class DisplayTranslatedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_translated);
 
         Intent intent = getIntent();
-        String message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
+        message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
@@ -33,20 +38,26 @@ public class DisplayTranslatedActivity extends AppCompatActivity {
 
     }
 
-    public void saveMessage(View view){
-        Intent intent = getIntent();
+    public void saveMessage(){
         try {
-            FileOutputStream fOut = openFileOutput("saved_data.txt",  MODE_APPEND);
-            OutputStreamWriter osw = new OutputStreamWriter(fOut);
-            osw.write("Fuck");
-            osw.flush();
-            osw.close();
-            fOut.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            //open file for writing
+            OutputStreamWriter out = new OutputStreamWriter(openFileOutput("save.txt", MODE_PRIVATE));
+
+            //write information to file
+            EditText text = (EditText)findViewById(R.id.message);
+            String text2 = text.getText().toString();
+            out.write(text2);
+            out.write('\n');
+
+            //close file
+
+            out.close();
+            Toast.makeText(this,"Text Saved",Toast.LENGTH_LONG).show();
+
+        } catch (java.io.IOException e) {
+            //if caught
+            Toast.makeText(this, "Text Could not be added",Toast.LENGTH_LONG).show();
         }
 
     }
-}
+
--- app/src/main/java/com/example/nat/essle/ViewSavedActivity.java
+++ app/src/main/java/com/example/nat/essle/ViewSavedActivity.java
@@ -2,12 +2,50 @@ package com.example.nat.essle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ViewSavedActivity extends AppCompatActivity {
+
+    private TextView textView;
+    private StringBuilder text = new StringBuilder();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_saved);
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("saved_data.txt")));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            while ((mLine = reader.readLine()) != null) {
+                text.append(mLine);
+                text.append('\n');
+            }
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Error reading file!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+            }
+
+            TextView output = (TextView) findViewById(R.id.textView);
+            output.setText((CharSequence) text);
+
+
+        }
     }
 }
