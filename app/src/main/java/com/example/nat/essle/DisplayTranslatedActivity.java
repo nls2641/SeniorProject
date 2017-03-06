bd97e724fed26d603562175457cac37ca89200da
Nat <nsrocds@gmail.com>
2017-03-05 22:05:39 -0800

    ?

--- app/src/main/java/com/example/nat/essle/DisplayTranslatedActivity.java
+++ app/src/main/java/com/example/nat/essle/DisplayTranslatedActivity.java
@@ -10,6 +10,11 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -23,12 +28,14 @@ public class DisplayTranslatedActivity extends AppCompatActivity {
 
 
     @Override
+
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_translated);
 
         Intent intent = getIntent();
-        message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
         TextView textView = new TextView(this);
         textView.setTextSize(40);
         textView.setText(message);
@@ -36,28 +43,12 @@ public class DisplayTranslatedActivity extends AppCompatActivity {
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_display_translated);
         layout.addView(textView);
 
-    }
-
-    public void saveMessage(){
-        try {
-            //open file for writing
-            OutputStreamWriter out = new OutputStreamWriter(openFileOutput("save.txt", MODE_PRIVATE));
-
-            //write information to file
-            EditText text = (EditText)findViewById(R.id.message);
-            String text2 = text.getText().toString();
-            out.write(text2);
-            out.write('\n');
-
-            //close file
-
-            out.close();
-            Toast.makeText(this,"Text Saved",Toast.LENGTH_LONG).show();
-
-        } catch (java.io.IOException e) {
-            //if caught
-            Toast.makeText(this, "Text Could not be added",Toast.LENGTH_LONG).show();
-        }
 
     }
 
+    public void saveMessage() {
+
+
+    }
+
+}
--- app/src/main/java/com/example/nat/essle/HomeActivity.java
+++ app/src/main/java/com/example/nat/essle/HomeActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+
 public class HomeActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
@@ -13,13 +17,24 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+
+
     }
 
     public void sendMessage(View view) {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+
         Intent intent = new Intent(this, DisplayTranslatedActivity.class);
         EditText editText = (EditText) findViewById(R.id.edit_text);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
+
+        myRef.setValue(message);
+
         startActivity(intent);
 
 
